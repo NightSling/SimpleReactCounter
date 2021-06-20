efb6b95a2ea7fd433a21d261b0d66a4ec44371ce
daysling <daysling@jeekie.host>
2021-06-20 22:45:15 +0545

    Added NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 class NavbarMain extends React.Component {
   render() {
     return (
     <div className="navbar">
-      <Navbar bg="dark">
-          
-      </Navbar>
+			<Navbar bg="dark" variant="dark">
+				<Navbar.Brand href="#">DayWeb</Navbar.Brand>
+				<Nav className="mr-auto">
+					<Nav.Link href="#">Home</Nav.Link>
+					<Nav.Link href="https://github.com/NightSling">GitHub</Nav.Link>
+					<Nav.Link href="https://twitter.com/dayslingthedev">Twitter</Nav.Link>
+				</Nav>
+			</Navbar>
     </div>
     )
   }
