97300c2612fbc0a61648856b8016c6136ff4dc98
daysling <daysling@jeekie.host>
2021-06-21 10:51:22 +0545

    I'm dumb, Compile error should be fixed (Changed class to className

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ class NavbarMain extends React.Component {
                 <a className="navbar-brand" href="#"><img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/></a>
                 <a className="form-inline my-2 my-lg-0">
                              <li className="nav-item active">
-                                     <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                                 </li>
                                 <li className="nav-item">
                                         <a className="nav-link" href="https://github.com/NightSling">GitHub</a>
