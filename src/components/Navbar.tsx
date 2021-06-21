5ef82aa89738056752a8c713358d188a462ab03a
daysling <daysling@jeekie.host>
2021-06-21 09:18:11 +0545

    Changed bg-ldark to bg-dark

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ class NavbarMain extends React.Component {
   render() {
     return (
     <div className="Navbar">
-        <nav className="navbar navbar-dark bg-ldark">
+        <nav className="navbar navbar-dark bg-dark">
                 <a className="navbar-brand" href="#">
-                        <img src="../../public/logo512.png" width="30" height="30" className="d-inline-block align-top" alt=""/>                 DayWeb
+                        <img src="../others/logo.png" width="30" height="30" className="d-inline-block align-top" alt=""/>                 DayWeb
                 </a>
         </nav>
     </div>
