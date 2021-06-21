9b3a31e85fb00792bda7e0ebe395d7ba26914acc
daysling <daysling@jeekie.host>
2021-06-21 08:22:29 +0545

    Fixed import path for css file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
-import '../css/Navbar';
+import '../css/Navbar.css';
 class NavbarMain extends React.Component {
   render() {
     return (
     <div className="navbar">
-			<Navbar bg="dark" variant="dark" expand="lg" >
+			<Navbar bg="dark" variant="dark" expand="lg">
 				<Navbar.Brand href="#">DayWeb</Navbar.Brand>
                                 <Navbar.Toggle aria-controls="navbarScroll" />
                                 <Navbar.Collapse id="navbarScroll" />
