4090bcbf76cf2813c77297748ae2200b998cf0a8
daysling <daysling@jeekie.host>
2021-06-21 10:53:58 +0545

    Removed <a>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,6 @@ class NavbarMain extends React.Component {
     <div className="Navbar">
         <nav className="navbar navbar-dark bg-dark">
                 <a className="navbar-brand" href="#"><img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/></a>
-                <a className="form-inline my-2 my-lg-0">
                              <li className="nav-item active">
                                      <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                                 </li>
@@ -17,7 +16,6 @@ class NavbarMain extends React.Component {
                                 <li className="nav-item">
                                         <a className="nav-link" href="https://twitter.com/dayslingthedev">Twitter</a>
                                 </li>
-                </a>
 
         </nav>
     </div>
