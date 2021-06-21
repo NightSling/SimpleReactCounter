40a57db15b7f4f30659c12d86e9a27370be1b135
daysling <daysling@jeekie.host>
2021-06-21 10:58:11 +0545

    Removed Lists

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,16 +6,9 @@ class NavbarMain extends React.Component {
     return (
     <div className="Navbar">
         <nav className="navbar navbar-dark bg-dark">
-                <a className="navbar-brand" href="#"><img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/></a>
-                             <li className="nav-item active">
-                                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                                </li>
-                                <li className="nav-item">
-                                        <a className="nav-link" href="https://github.com/NightSling">GitHub</a>
-                                </li>
-                                <li className="nav-item">
-                                        <a className="nav-link" href="https://twitter.com/dayslingthedev">Twitter</a>
-                                </li>
+                <a className="navbar-brand" href="#"><img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/>DayWeb</a>
+                             <a className="nav-link" href="https://github.com/NightSling">GitHub</a>
+                             <a className="nav-link" href="https://twitter.com/dayslingthedev">Twitter</a>
 
         </nav>
     </div>
