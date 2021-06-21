cfede22a945452912ee18e390cd80c2e130733f8
daysling <daysling@jeekie.host>
2021-06-21 09:00:14 +0545

    Added a snippet

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,17 +5,16 @@ import '../css/Navbar.css';
 class NavbarMain extends React.Component {
   render() {
     return (
-    <div className="navbar">
-			<Navbar bg="dark" variant="dark" expand="lg">
-				<Navbar.Brand href="#">DayWeb</Navbar.Brand>
-                                <Navbar.Toggle aria-controls="navbarScroll" />
-                                <Navbar.Collapse id="navbarScroll" />
-                                <Nav className="mr-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-					<Nav.Link href="#">Home</Nav.Link>
-					<Nav.Link href="https://github.com/NightSling">GitHub</Nav.Link>
-					<Nav.Link href="https://twitter.com/dayslingthedev">Twitter</Nav.Link>
-				</Nav>
-			</Navbar>
+    <div className="Navbar">
+        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                            <Navbar.Brand href="#">DayWeb</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Nav className="mr-auto">
+                                    <Nav.Link href="https://github.com/NightSling">GitHub</Nav.Link>
+                                    <Nav.Link href="https://twitter.com/dayslingthedev">Twitter</Nav.Link>
+                                    <Nav.Link href="/contact-us">About Us</Nav.Link>
+                            </Nav>
+        </Navbar>
     </div>
     )
   }
