5cf35e6a67b37cc550d4bfbd6862dbdd7620c3f3
daysling <daysling@jeekie.host>
2021-06-21 09:07:40 +0545

    Changed to plain bootstrap

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav } from 'react-bootstrap';
-import '../css/Navbar.css';
 class NavbarMain extends React.Component {
   render() {
     return (
     <div className="Navbar">
-        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                            <Navbar.Brand href="#">DayWeb</Navbar.Brand>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Nav className="mr-auto">
-                                    <Nav.Link href="https://github.com/NightSling">GitHub</Nav.Link>
-                                    <Nav.Link href="https://twitter.com/dayslingthedev">Twitter</Nav.Link>
-                                    <Nav.Link href="/contact-us">About Us</Nav.Link>
-                            </Nav>
-        </Navbar>
+        <nav className="navbar navbar-dark bg-ldark">
+                <a className="navbar-brand" href="#">
+                        <img src="../../public/logo512.png" width="30" height="30" className="d-inline-block align-top" alt=""/>                 DayWeb
+                </a>
+        </nav>
     </div>
     )
   }
