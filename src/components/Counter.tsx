c1304be0998a384e30c023005296c50bd2cdcaba
daysling <daysling@jeekie.host>
2021-06-24 20:28:44 +0545

    Warrped Button in Div

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,8 +7,8 @@ class CounterClass extends Component {
             <div className="Counter">
                 <h1>{counter}</h1>
                 <h2>Counter</h2>
-            </div>
             <button onClick={setCounter(counter + 1)}>Click Me!</button>
+            </div>
         );
     }
 }
