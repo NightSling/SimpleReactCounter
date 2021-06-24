a770e02649cdc7b9a72d4c3a7342bbf56a2f4c9b
daysling <daysling@jeekie.host>
2021-06-24 22:17:58 +0545

    Changed Styling

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,13 @@
 import { Component, useState } from "react";
+import '../css/Counter.css';
 
 const CounterClass = () => {
     const [counter, setCounter] = useState(0);
     return (
-        <div className="Counter">
-            <h1>{counter}</h1>
-            <h2>Counter</h2>
-            <button onClick={ () => setCounter(counter + 1) }>Click Me!</button>
+        <div className="Counter text-center margin-5">
+            <h1 className="CountText mb-0">{counter}</h1>
+            <h3 className="Count blockquote-footer">Counter</h3>
+            <button type="button" className="Count1 btn btn-success" onClick={ () => setCounter(counter + 1) }>Click Me!</button>
         </div>
     );
 };
