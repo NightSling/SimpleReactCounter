25c1c77175677259bb52e4121d6d49a216510267
daysling <daysling@jeekie.host>
2021-06-24 20:40:25 +0545

    Made useState() through wrapper class

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,22 @@
 import {Component, useState} from 'react';
 
+const [counter, setCounter] = useState(0);
+
+function getCounter() : number{
+    return counter;
+}
+
+function setCount(ah: number) : any { 
+    setCounter(ah);
+}
+
 class CounterClass extends Component {
     render() {
-        let [counter, setCounter] = useState(0);
         return (
             <div className="Counter">
-                <h1>{counter}</h1>
+                <h1>{getCounter()}</h1>
                 <h2>Counter</h2>
-            <button onClick={setCounter(counter + 1)}>Click Me!</button>
+                <button onClick={setCount(getCounter() + 1)}>Click Me!</button>
             </div>
         );
     }
