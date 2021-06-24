e49d04b4baa771bca44385feb0e866981a9506ba
daysling <daysling@jeekie.host>
2021-06-24 21:07:48 +0545

    Made Counter actually work

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,24 +1,14 @@
 import { Component, useState } from "react";
 
-
-const createCount = () => {
+const CounterClass = () => {
     const [counter, setCounter] = useState(0);
-    const updateCounter = (value : number) : any => {
-        setCounter(counter + value);
-    }
     return (
         <div className="Counter">
             <h1>{counter}</h1>
             <h2>Counter</h2>
-            <button onClick={updateCounter(1)}>Click Me!</button>
+            <button onClick={ () => setCounter(counter + 1) }>Click Me!</button>
         </div>
     );
 };
-
-class CounterClass extends Component {
-    render() {
-        return createCount();
-    }
-}
 
 export default CounterClass;
