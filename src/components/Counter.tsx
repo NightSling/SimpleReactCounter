597e6c75621dce7fee96b88297e53e495178df1e
daysling <daysling@jeekie.host>
2021-06-24 20:52:00 +0545

    Moved from top level functions to local const based

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,24 +1,23 @@
-import {Component, useState} from 'react';
+import { Component, useState } from "react";
 
-const [counter, setCounter] = useState(0);
 
-function getCounter() : number{
-    return counter;
-}
-
-function setCount(ah: number) : any { 
-    setCounter(ah);
-}
+const createCount = () => {
+    const [counter, setCounter] = useState(0);
+    const updateCounter = (value : number) : any => {
+        setCounter(counter + value);
+    }
+    return (
+        <div className="Counter">
+            <h1>{counter}</h1>
+            <h2>Counter</h2>
+            <button onClick={updateCounter(1)}>Click Me!</button>
+        </div>
+    );
+};
 
 class CounterClass extends Component {
     render() {
-        return (
-            <div className="Counter">
-                <h1>{getCounter()}</h1>
-                <h2>Counter</h2>
-                <button onClick={setCount(getCounter() + 1)}>Click Me!</button>
-            </div>
-        );
+        return createCount();
     }
 }
 
